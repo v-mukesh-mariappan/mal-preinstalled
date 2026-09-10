62ca3e4fc375efc9c32df02f542102fcf8ec5d5b
v-mukesh-mariappan <147705302+v-mukesh-mariappan@users.noreply.github.com>
2026-09-10 07:49:02 +0000

    add second program

---
+++ script/second.py
@@ -0,0 +1,13 @@
+import json
+from urllib.request import urlopen
+
+
+def main():
+	"""Make a simple GET request and print the response."""
+	with urlopen("https://jsonplaceholder.typicode.com/todos/1", timeout=10) as response:
+		data = json.load(response)
+	print(data)
+
+
+if __name__ == "__main__":
+	main()
